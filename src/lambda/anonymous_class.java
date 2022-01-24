fcbc220c45158b6f00e6f0a0a893dc23645bd0b2
defr0stt <dima.sb713@gmail.com>
2022-01-24 19:20:35 +0200

    Anonymous classes

---
+++ src/lambda/anonymous_class.java
@@ -0,0 +1,24 @@
+package lambda;
+
+public class anonymous_class
+{
+    public void eat(){
+        System.out.println("I'm eating...");
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        anonymous_class anonymousClass = new anonymous_class();
+
+        anonymous_class anonymousClass1 = new anonymous_class(){    // successor of anonymous_class class
+            @Override                                               // it can override methods whereas to
+            public void eat() {                                     // create new class
+                System.out.println("You are eating...");;
+            }
+        };
+
+        anonymousClass.eat();
+        anonymousClass1.eat();
+    }
+}
